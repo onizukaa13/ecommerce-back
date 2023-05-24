3677b4d11d3992a1f602c046b1420636d4238889
onizukaa13 <75269274+onizukaa13@users.noreply.github.com>
2023-05-24 16:21:53 +0200

    Page principal affiche les livres + image + ajoute au panier+ voir details du livre +supression du panier

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,22 +12,22 @@ class AppFixtures extends Fixture
     {
         $books = [
             [
-                'title' => 'Le Seigneur des Anneaux',
-                'description' => 'Une épopée fantastique',
-                'author' => 'J.R.R. Tolkien',
-                'price' => 19.99,
-                'image' => '...',
-                'isbn' => '1234567890',
+                'title' => 'Symfony pour les développeurs',
+                'description' => 'Un guide complet pour apprendre Symfony',
+                'author' => 'John Smith',
+                'price' => 29.99,
+                'image' => 'https://example.com/images/symfony_for_developers.jpg',
+                'isbn' => '9783456789012',
                 'stock' => 'Disponible',
-                'genre' => 'Fantasy',
-                'format' => 'Poche'
+                'genre' => 'Informatique',
+                'format' => 'Broché'
             ],
             [
                 'title' => 'Les Misérables',
                 'description' => 'Un roman historique',
                 'author' => 'Victor Hugo',
                 'price' => 14.99,
-                'image' => '...',
+                'image' => 'https://cdn1.booknode.com/book_cover/83/les_miserables-83116-264-432.jpg',
                 'isbn' => '9782070404672',
                 'stock' => 'Disponible',
                 'genre' => 'Roman historique',
@@ -38,7 +38,7 @@ class AppFixtures extends Fixture
                 'description' => "Un roman d'aventure et de vengeance",
                 'author' => 'Alexandre Dumas',
                 'price' => 12.50,
-                'image' => '...',
+                'image' => 'https://media.groupe.gallimard.fr/couvHD/A64513.jpg',
                 'isbn' => '9782070416705',
                 'stock' => 'Disponible',
                 'genre' => "Roman d'aventure",
@@ -47,10 +47,10 @@ class AppFixtures extends Fixture
 
             [
                 'title' =>'Harry Potter',
-                'description' => 'Un roman fantastique rempli de magie et d\aventure',
+                'description' => 'Un roman fantastique rempli de magie et d\'aventure',
                 'author' => 'J.K. Rowling',
                 'price' =>  15.99,
-                'image' => '...',
+                'image' => 'https://m.media-amazon.com/images/I/813JfJQwefL.jpg',
                 'isbn' => '9782070584622',
                 'stock' => 'Disponible',
                 'genre' => 'Roman fantastique',
@@ -62,12 +62,36 @@ class AppFixtures extends Fixture
                 'description' => 'Un roman d\'espionnage captivant',
                 'author' => 'Robert Muchamore',
                 'price' => 10.99,
-                'image' => '...',
+                'image' => 'https://cdn1.booknode.com/book_cover/1393/cherub_tome_2_trafic-1393090-264-432.jpg',
                 'isbn' => '9782203133158',
                 'stock' => 'Disponible',
                 'genre' => 'Roman d\espionnage',
                 'format' => 'Poche',
             ],
+
+            [   'title' => 'Encyclopédie des animaux',
+                'description' => 'Découvrez le monde fascinant des animaux',
+                'author' => 'John Doe',
+                'price' => 19.99,
+                'image' => 'https://example.com/images/encyclopedia_of_animals.jpg',
+                'isbn' => '9781234567890',
+                'stock' => 'Disponible',
+                'genre' => 'Livre animalier',
+                'format' => 'Relié'
+            ],
+
+            [   'title' => 'Le Petit Prince',
+                'description' => 'Un conte poétique et philosophique',
+                'author' => 'Antoine de Saint-Exupéry',
+                'price' => 9.99,
+                'image' => 'https://example.com/images/le_petit_prince.jpg',
+                'isbn' => '9782345678901',
+                'stock' => 'Disponible',
+                'genre' => 'Conte',
+                'format' => 'Broché'
+            ]
+
+
 
 
           
